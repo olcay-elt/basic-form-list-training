a4dee9cbbb4444c2051ffff2ceb025b6d0ca79f2
olcay-elt <olcay.elt@gmail.com>
2023-03-25 11:28:21 +0100

    Add

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react'
+
 import Form from './Form'
 import List from "./List"
 function Contacts() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            fullname: "Ahmet",
+            phone_number: "123123"
+        }, {
+            fullname: "Mehmet",
+            phone_number: "456456"
+        }
+    ]);
 
     useEffect(() => {
         console.log(contacts);
