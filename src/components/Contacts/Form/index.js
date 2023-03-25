29dadd6a3dac2c8a51fc9a939a7bc8853d420e12
olcay-elt <olcay.elt@gmail.com>
2023-03-25 13:47:55 +0100

    Add

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const initialFormValues = { fullname: "", phone_number: "" };
 
-function Form({ addContact, contcts }) {
+function Form({ addContact, contacts }) {
     const [form, setForm] = useState(initialFormValues);
 
     const onChangeInput = (e) => {
